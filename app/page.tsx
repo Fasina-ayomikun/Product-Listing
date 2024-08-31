089af523b61783f7905b1baaa1f424b9624629f1
fasina-ayomikun <ayomikunfasina@yahoo.com>
2024-08-31 04:19:06 +0100

    fixed localstorage error

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import CreateProductModal from "../components/CreateProductModal";
 import { defaultProducts } from "../utils/constants";
-import { useEffect } from "react";
+import { Suspense, useEffect } from "react";
 import Header from "../components/Header";
 import Hero from "@/components/Hero";
 import Filters from "@/components/Filters";
@@ -34,28 +34,30 @@ export default function Home() {
     }
   }, []);
   return (
-    <section className='relative'>
-      <Header />
-      <Hero />
+    <Suspense fallback={<p>Loading..</p>}>
+      <section className='relative'>
+        <Header />
+        <Hero />
 
-      <section className='mt-7 container'>
-        <h1 className='text-2xl font-semibold my-4 capitalize underline  text-center text-purple-200'>
-          Our Products
-        </h1>
-        <Filters setCategory={setCategory} setPrice={setPrice} />
+        <section className='mt-7 container'>
+          <h1 className='text-2xl font-semibold my-4 capitalize underline  text-center text-purple-200'>
+            Our Products
+          </h1>
+          <Filters setCategory={setCategory} setPrice={setPrice} />
+        </section>
+        <section className='grid md:grid-cols-2 grid-cols-1 lg:grid-cols-3 items-center gap-8 container px-10 py-4'>
+          {filteredProducts.length < 1 ? (
+            <p className='text-pink-200'>
+              No product available, create new products
+            </p>
+          ) : (
+            filteredProducts.map((product) => {
+              return <SingleProduct key={product.id} product={product} />;
+            })
+          )}
+        </section>
+        <CreateProductModal isEditing={false} id={""} setIsEditing={() => {}} />
       </section>
-      <section className='grid md:grid-cols-2 grid-cols-1 lg:grid-cols-3 items-center gap-8 container px-10 py-4'>
-        {filteredProducts.length < 1 ? (
-          <p className='text-pink-200'>
-            No product available, create new products
-          </p>
-        ) : (
-          filteredProducts.map((product) => {
-            return <SingleProduct key={product.id} product={product} />;
-          })
-        )}
-      </section>
-      <CreateProductModal isEditing={false} id={""} setIsEditing={() => {}} />
-    </section>
+    </Suspense>
   );
 }
